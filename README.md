7e6f257f27ab595a4deebf8377c3e015595bdb2b
Katoragashua <katoragashua@outlook.com>
2025-01-31 15:45:46 +0100

    HNG-Assignment 0: Made modifications to responses at the home route and /katoragashua. Also made changes in the README file.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# katoragashua_hngstage_0
+# HNG Internship Stage 0 Assignment
+This is a simple Node.js application that returns a the basic information of the intern.
+
+## Dependencies used
+* Express
+* Cors
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/katoragashua/katoragashua_hngstage_0.git
--- app.js
+++ app.js
@@ -10,15 +10,19 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "My name is Kator Agashua." });
+  res
+    .status(200)
+    .send(
+      `<h1>Welcome to my HNG Internship Stage 0 assignment Page.<br/>Click <a href="/katoragashua">here</a> to visit the assignment page. </h1>`
+    );
 });
 
 app.get("/katoragashua", (req, res) => {
-    res.status(200).json({
-        email: "katoragashua@gmail.com",
-        current_datetime: new Date().toISOString(),
-        github_url: "https://github.com/katoragashua"    
-    })
-})
+  res.status(200).json({
+    email: "katoragashua@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: "https://github.com/katoragashua/katoragashua_hngstage_0",
+  });
+});
 
 module.exports = app;
